b63c579bd2bfd5a1ac3c69d1e9c7a77d403d9dda
Ho Nyeong <dev.honing@gmail.com>
2024-03-26 16:58:42 +0900

    Refactor: 항목별로 코드 정리

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import cv2
 import numpy as np
 
 # 이미지 불러오기
-path = './3dog.png'
-image = cv2.imread(path)
+image_path = './3dog.png'
+image = cv2.imread(image_path)
 
-# 이미지를 그레이스케일로 변환
+# 그레이스케일링
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # 가우시안 블러(노이즈 제거)
@@ -18,10 +18,9 @@ edges = cv2.Canny(blurred, 50, 150)
 # 컨투어 찾기
 contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# 컨투어 그리기
-cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+# 초록색 외곽선 그리기
+green_contours_image = image.copy()
+cv2.drawContours(green_contours_image, contours, -1, (0, 255, 0), 2)
 
-# 결과 이미지 출력
-cv2.imshow('Outline Detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# 이미지 저장
+cv2.imwrite('green_contours_image.png', green_contours_image)
