c5e5c3a0f1cea37eea62a3857bf914202a4fb7bd
Ho Nyeong <dev.honing@gmail.com>
2024-03-26 16:33:26 +0900

    Feat: 컨투어를 찾아 그리기

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
 # 케니 엣지 검출
 edges = cv2.Canny(blurred, 50, 150)
+
+# 컨투어 찾기
+contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# 컨투어 그리기
+cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
