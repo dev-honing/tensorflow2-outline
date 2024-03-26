2e94c3338d08f7a14619a240145706cc23505c6b
Ho Nyeong <dev.honing@gmail.com>
2024-03-26 16:33:08 +0900

    Feat: 노이즈 제거 및 엣지 검출

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ image = cv2.imread(path)
 
 # 이미지를 그레이스케일로 변환
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# 가우시안 블러(노이즈 제거)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# 케니 엣지 검출
+edges = cv2.Canny(blurred, 50, 150)
