b1eb1bf39e29c67a2e82d7434f06a38337a3b0d4
Ho Nyeong <dev.honing@gmail.com>
2024-04-07 23:54:07 +0900

    Test: 패키지 설치 확인을 위한 출력 확인

---
+++ Tensorflow/main.py
@@ -0,0 +1,10 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+# Tensorflow 설치 확인
+print(tf.__version__) # 2.16.1
+
+# matplotlib 설치 확인
+print(matplotlib.__version__) # 3.8.4
